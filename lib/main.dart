541c9fff0462cde68cfa2b36af3aea11b88d6e43
Adam0999 <eed741@hotmail.com>
2022-09-26 12:09:52 +0700

    1

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:shop_apps/screens/splash/splash_screen.dart';
+import 'package:shop_apps/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Shop Apps Demo",
+      theme: theme(),
+      home: SplashScreen(),
+      // initialRoute: SplashScreen.routeName,
+      // routes: routes,
+    );
+  }
+}
